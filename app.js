c829e59d7433a3949e57cc56bc20e1a98c4d26be
Leah Miller <leahmiller1969@gmail.com>
2021-04-07 20:51:52 -0700

    truncate posts using ejs // challenge 20

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ const contactContent =
 
 const app = express();
 
-let posts = [];
+let posts = [
+  {
+    title: "Post",
+    body:
+      "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.",
+  },
+];
 
 app.set("view engine", "ejs");
 
@@ -22,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
+  console.log(posts, "posts in home");
   res.render("home.ejs", {
     content: homeStartingContent,
     posts: posts,
